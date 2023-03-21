d8831127e58f5783a74576b694b5b73f8bd364a7
Xuechen Liu <xuechenliu1005@gmail.com>
2023-03-21 16:39:58 +0200

    we'll see this one

--- sandbox/data/asvspoof2021/create_score_file_for_convert.py
+++ sandbox/data/asvspoof2021/create_score_file_for_convert.py
@@ -3,18 +3,21 @@ import sys
 
 
 if __name__ == "__main__":
-    input_score = "ASVspoof2021.LA.asv.eval.gi.trl.scores.txt"
-    eval_protocol = "protocol.txt"
+    input_score = "score.txt"
+    eval_protocol = "trial_metadata.txt"
+    target_score_file = "ASVspoof2021.LA.asv.eval.gi.trl.scores.txt"
 
     score_pairs = {}
     with open(input_score, "r") as inps:
         for line in inps:
             full_spk, full_utt, score = line.split()
-            utt = full_utt.split("-")[1]
-            score_pairs[utt] = score
+            score_pairs[(full_spk, full_utt)] = score
 
-    with open(eval_protocol, "r") as evp, open("asv_scores.txt", "w") as ass:
+    with open(eval_protocol, "r") as evp, open(target_score_file, "w") as ass:
         for line in evp:
-            _, utt, _, attack, decision = line.split()
-            score = score_pairs[utt]
+            # LA_0007-alaw-ita_tx LA_E_5013670-alaw-ita_tx alaw ita_tx bonafide nontarget notrim eval
+            full_spk, full_utt, _, _, attack, decision, _, type_set = line.split()
+            if type_set == "progress":
+                continue
+            score = score_pairs[(full_spk, full_utt)]
             ass.write("{0} {1} {2}\n".format(attack, decision, score))
