4dc45d2e7410e7e8fdd3c98bf5004c9b8d2a0882
Xuechen Liu <xuechenliu1005@gmail.com>
2023-03-27 11:54:16 +0300

    add data preparation script for aug study

---
+++ project/03-asvspoof-mega/00_prepare_aug_data.py
@@ -0,0 +1,23 @@
+# Prepare the data directories for augmentation experiment
+# Just split the annotated wav files into different sub-directories
+import glob
+import os
+import sys
+
+from shutil import copyfile
+
+
+if __name__ == "__main__":
+    src_augdata_dir = "aug_data/data"
+    tar_augdata_dir = "DATA/asvspoof2019_LA"
+    aug_sources = ["none", "reverb", "noise", "mp3"]
+
+    for aug_src in aug_sources:
+        aug_src_files = glob.glob(src_augdata_dir + "/*-{0}.flac".format(aug_src))
+        os.makedirs(tar_augdata_dir + "/train_dev_{0}".format(aug_src))
+        for item in aug_src_files:
+            utt_name = os.path.basename(item).split("-")[0]
+            copyfile(
+                item,
+                tar_augdata_dir + "/train_dev_{0}/{1}.flac".format(aug_src, utt_name),
+            )
