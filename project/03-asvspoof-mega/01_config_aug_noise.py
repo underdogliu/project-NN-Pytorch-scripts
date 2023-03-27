f814305cb1917e91fc457c4530946eca7620dd85
Xuechen Liu <xuechenliu1005@gmail.com>
2023-03-27 13:21:57 +0300

    case by case

--- project/03-asvspoof-mega/01_config_aug_noise.py
+++ project/03-asvspoof-mega/01_config_aug_noise.py
@@ -17,26 +17,26 @@ __copyright__ = "Copyright 2020, Xin Wang"
 #########################################################
 
 # Name of datasets (any string you wish to use)
-#  after data preparation, trn/val_set_name are used to save statistics 
+#  after data preparation, trn/val_set_name are used to save statistics
 #  about the data sets
-trn_set_name = 'asvspoof2019_trn'
-val_set_name = 'asvspoof2019_val'
+trn_set_name = "asvspoof2019_trn"
+val_set_name = "asvspoof2019_val"
 
 # for convenience
 #  we will use resources in this directory
-tmp = os.path.dirname(__file__) + '/../../DATA/asvspoof2019_LA'
+tmp = os.path.dirname(__file__) + "/../../DATA/asvspoof2019_LA"
 
 # File lists (text file, one data name per line, without name extension)
 # trin_file_list: list of files for training set
-trn_list = tmp + '/scp/train.lst'  
+trn_list = tmp + "/scp/train.lst"
 # val_file_list: list of files for validation set. It can be None
-val_list = tmp + '/scp/val.lst'
+val_list = tmp + "/scp/val.lst"
 
 # Directories for input features
 # input_dirs = [path_of_feature_1, path_of_feature_2, ..., ]
 #  we assume train and validation data are put in the same sub-directory
 # input_dirs = [tmp + '/train_dev']
-input_dirs = [tmp + '/train_dev' + '/noise']
+input_dirs = [tmp + "/train_dev_noise"]
 
 # Dimensions of input features
 # input_dims = [dimension_of_feature_1, dimension_of_feature_2, ...]
@@ -45,23 +45,23 @@ input_dims = [1]
 # File name extension for input features
 # input_exts = [name_extention_of_feature_1, ...]
 #  If you have waveform in *.flac, please use input_exts = ['.flac']
-input_exts = ['.flac']
+input_exts = [".flac"]
 
 # Temporal resolution for input features
 # input_reso = [reso_feature_1, reso_feature_2, ...]
 #  for waveform modeling, temporal resolution of input acoustic features
 #  may be = waveform_sampling_rate * frame_shift_of_acoustic_features
-#  for example, 80 = 16000 Hz * 5 ms 
+#  for example, 80 = 16000 Hz * 5 ms
 input_reso = [1]
 
 # Whether input features should be z-normalized
 # input_norm = [normalize_feature_1, normalize_feature_2]
 input_norm = [False]
-    
+
 # Similar configurations for output features
 output_dirs = []
 output_dims = [1]
-output_exts = ['.bin']
+output_exts = [".bin"]
 output_reso = [1]
 output_norm = [False]
 
@@ -78,12 +78,12 @@ truncate_seq = None
 #  If sequence length < minimum_len, this sequence is not used for training
 #  minimum_len can be None
 minimum_len = None
-    
+
 
 # Optional argument
 #  Just a buffer for convenience
 #  It can contain anything
-optional_argument = [tmp + '/protocol.txt']
+optional_argument = [tmp + "/protocol.txt"]
 
 #########################################################
 ## Configuration for inference stage
@@ -91,17 +91,16 @@ optional_argument = [tmp + '/protocol.txt']
 # similar options to training stage
 
 
-test_set_name = 'asvspoof2019_test'
+test_set_name = "asvspoof2019_test"
 
 # List of test set data
 # for convenience, you may directly load test_set list here
-test_list = tmp + '/scp/test.lst'
+test_list = tmp + "/scp/test.lst"
 
 # Directories for input features
 # input_dirs = [path_of_feature_1, path_of_feature_2, ..., ]
 #  we assume train and validation data are put in the same sub-directory
-test_input_dirs = [tmp + '/eval']
+test_input_dirs = [tmp + "/eval"]
 
 # Directories for output features, which are []
 test_output_dirs = []
-
